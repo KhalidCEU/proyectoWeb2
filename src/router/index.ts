349d3b945f9ab84a9a2dde5d0e29505a8643b041
Trjeas <trjeas@gmv.com>
2025-05-15 12:40:29 +0200

    Stores added to db

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import sneakersRouter from './sneakers';
+import storesRouter from './stores';
 
 const router = express.Router()
 
 router.use('/sneakers', sneakersRouter);
+router.use('/stores', storesRouter);
 
 
 export default router;
