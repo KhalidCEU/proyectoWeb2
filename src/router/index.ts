11c19505c8cde7ec0aecf28dec5c8a2c5b3b9c01
kike454 <enriquecomu5@gmail.com>
2025-05-18 18:06:58 +0200

    feat: add providers

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import sneakersRouter from './sneakers';
 import storesRouter from './stores';
+import providersRouter from './provider';
 
 const router = express.Router()
 
 router.use('/sneakers', sneakersRouter);
 router.use('/stores', storesRouter);
+router.use('/providers', providersRouter);
 
 
 export default router;
