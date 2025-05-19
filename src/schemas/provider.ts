478ff04e6be653fb3ffa8450e528e6d522a23ab0
pablogaraay <garayperezpablo@gmail.com>
2025-05-19 13:40:12 +0200

    provider schema email validation fixed

--- src/schemas/provider.ts
+++ src/schemas/provider.ts
@@ -1,9 +1,10 @@
 import mongoose from "mongoose";
+import isEmail from 'validator/lib/isEmail';
 
 const providerSchema = new mongoose.Schema({
     _id: { type: String },
     name: { type: String, required: true },
-    contact_email: { type: String, required: true }
+    contact_email: { type: String, required: true, validate: {validator: (v: string) => isEmail(v)}}
 });
 
 export const Provider = mongoose.model('Provider', providerSchema);
