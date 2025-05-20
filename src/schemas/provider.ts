f5361738ad8bd9862b08673e2f1380d7f15860c0
khalidbelk <khalid.belkassmi-el-hafi@epitech.eu>
2025-05-20 18:09:29 +0200

    fix(providers): remove _id from mongoose schema fields

--- src/schemas/provider.ts
+++ src/schemas/provider.ts
@@ -1,7 +1,6 @@
 import mongoose from "mongoose";
 
 const providerSchema = new mongoose.Schema({
-    _id: { type: String },
     name: { type: String, required: true },
     contact_email: { type: String, required: true}
 });
